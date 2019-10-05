7875e0ccf904e1f9dd705c96eac57f988b6c426a
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-10-04 20:10:54 -0400

    delete file is done

--- api/secure_api.py
+++ api/secure_api.py
@@ -1,4 +1,4 @@
-import json, requests, os
+import json
 import security.jwtSecurity
 from api.public_api import search_a_book
 from flask import Blueprint, request
@@ -28,31 +28,41 @@ def upload_book(file_name):
     with open(file_name, 'rb') as my_file:
         blob.upload_from_file(my_file)
 
-#########################################################
 
-# @secure_api.route("/update", methods=['PUT'])
+
+@secure_api.route("/delete", methods=['DELETE'])
 # @security.jwtSecurity.requires_auth
-# def update_books():
-#     # http://127.0.0.1:5000/secure/add
-#     """
-#     param: book_name
-#     :return: 'success' or 'error'
-#     """
-#     book_name = request.args.get("book_name")
-#     upload_book(book_name)
-#     return json.dumps({'success': True})
-#
-#
-# @secure_api.route("/delete", methods=['DELETE'])
-# @security.jwtSecurity.requires_auth
-# def delete_books():
-#     # http://127.0.0.1:5000/secure/delete
-#     """
-#     param: book_name
-#     :return: 'success' or 'error'
-#     """
-#     book_name = request.args.get("book_name")
-#     delete_book(book_name)
-#     return json.dumps({'success': True})
+def delete_books():
+    # http://127.0.0.1:5000/secure/delete
+    """
+    param: book_name
+    :return: 'success' or 'error'
+    """
+    book_name = request.args.get("book_name")
+    try:
+        delete_blob(book_name)
+        return json.dumps({'success': True})
+    except Exception as e:
+        print(e)
+        return json.dumps({"error":"404 not found"})
 
 
+
+def delete_blob(blob_name):
+    """Deletes a blob from the bucket."""
+    blob = bucket.blob(blob_name)
+    blob.delete()
+    print('Blob {} deleted.'.format(blob_name))
+
+
+# def download_blob(bucket_name, source_blob_name, destination_file_name):
+#     """Downloads a blob from the bucket."""
+#     storage_client = storage.Client()
+#     bucket = storage_client.get_bucket(bucket_name)
+#     blob = bucket.blob(source_blob_name)
+#
+#     blob.download_to_filename(destination_file_name)
+#
+#     print('Blob {} downloaded to {}.'.format(
+#         source_blob_name,
+#         destination_file_name))
